a2666c74c16e7f78840fba72ca4e99e1bde3f301
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-31 02:44:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,7 +28,9 @@ def num_points_scored(name)
     team_data.each do |attribute, data|
       if attribute == :players
         data.each do |name, stat|
-          
+          if name = "#{name}"
+            stat.fetch(:points)
+          end
         end
       end
 
